334b15e544fc5df44327672efc0ee95d47bd25a6
Gian Carlos Cuello <giancarlos.cuello@gmail.com>
2019-05-14 14:04:26 -0500

    Refractor code

--- multi-hosts-userinput.py
+++ multi-hosts-userinput.py
@@ -1,5 +1,6 @@
 from mininet.topo import Topo
 
+
 class MyTopo(Topo):
     "Creating user input topology example."
 
@@ -8,22 +9,28 @@ class MyTopo(Topo):
         # Initialize topology
         Topo.__init__(self)
         # user input
-        input_num = input("Enter your number hosts.")
+        input_num = input("Enter your number hosts: ")
         conv_toint = int(input_num)
         "The number hosts is:" + str(conv_toint)
         hosts = []
         switches = []
-        for i in range(0, conv_toint):
-            hosts.append(self.addHost('h' + str(i)))
-            "Hosts added " + hosts[i]
-        input_swts = input("Enter number of Switches")
-        swt_toint = int(0, input_swts)
+        for i in range(conv_toint):
+            hosts.append(self.addHost('h' + `i`))
+            print "Hosts added " + hosts[i]
+
+        input_swts = input("Enter number of Switches: ")
+        swt_toint = int(input_swts)
+        print "Switch added "+str(swt_toint)
+
         for j in range(swt_toint):
-            switches.append(self.addSwitch('s' + str(j)))
-            "Switch added" + switches[j]
+            switches.append(self.addSwitch('s' + `j`))
+            print "Switch added" + switches[j]
+
         "Creating links by user"
-        for k in range(0, swt_toint):
-            "Add the links to each switch."
-            "Example: 'h1 -> s1'"
+
+        for k in range(swt_toint):
+            print "Add the links to each switch."
+            print "Example: 'h1 -> s1'"
+
 
 topos = {'mytopo': (lambda: MyTopo())}
